8f73e59e5714f00775fc683bc5bdf1e556f70793
Chinmay Khaladkar <cbkhalad@gmail.com>
2026-08-26 17:16:29 -0700

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        unordered_set<ListNode*> seen;
+        for (ListNode* cur = head; cur != nullptr; cur = cur->next) {
+            if (seen.contains(cur)) return true;
+            seen.insert(cur);
+        }
+
+        return false;
+    }
+};
